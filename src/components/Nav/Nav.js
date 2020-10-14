7939ea6fa9806a45603fe3ce6c8e26febf612caa
Nicholas Otieno <niklasoti@gmail.com>
2020-10-14 15:06:01 +0200

    Fix Memory Leaks & Update Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,43 +9,43 @@ class Nav extends React.Component {
 
   sections = sections;
 
-  componentDidMount(){
+  componentDidMount() {
     this.updateTitle();
-    this.appDrawer = new MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));     
+    this.appDrawer = new MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
     this.simpleMenu = new MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
 
     /** Temporary Hack... onclick does not work on the jsx template*/
     const list = Array.from(document.querySelectorAll('.list-item'));
     list.forEach(item => item.addEventListener('click', e => this.switch(e)));
   }
-  
-  toggleDrawer(){
+
+  toggleDrawer() {
     this.appDrawer.open = !this.appDrawer.open;
   }
 
-  toggleMenu(){
+  toggleMenu() {
     this.simpleMenu.open = !this.simpleMenu.open;
   }
 
-  updateTitle(){
-     
+  updateTitle() {
+
     let sectionId;
-    
-    if(window.location.pathname !== '/detail'){
+
+    if (window.location.pathname !== '/detail') {
       sectionId = window.location.hash.slice(1) || 'world';
-    }else{
+    } else {
       let str = window.location.hash.slice(1);
       let n = str.indexOf('/');
       sectionId = str.substring(0, n !== -1 ? n : str.length);
     }
 
     const obj = this.sections.find(el => el['id'] === sectionId)
-    this.setState({title : obj.webTitle.toUpperCase()})
+    this.setState({ title: obj.webTitle.toUpperCase() })
   }
 
-  switch(e){
+  switch(e) {
     this.toggleDrawer();
-    this.setState({title : e.target.innerText.toUpperCase()});
+    this.setState({ title: e.target.innerText.toUpperCase() });
     window.scrollTo(0, 0);
   }
 
@@ -53,12 +53,12 @@ class Nav extends React.Component {
     if (navigator.share) {
       navigator.share({
         title: 'News App',
-        text: 'Check out this news app — it rocks!',
+        text: 'Check out this news app',
         url: 'https://pwa-news-app.netlify.com',
       })
-      .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing', error));
-    }else {
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    } else {
       window.open(
         'https://twitter.com/intent/tweet?text=https://pwa-news-app.netlify.com',
         '_blank'
@@ -70,11 +70,11 @@ class Nav extends React.Component {
 
     const listItems = this.sections.map((item) => {
       return (
-        <div key={item.id}> 
+        <div key={item.id}>
           <a className="mdc-list-item list-item" href={`#${item.id}`}>
             {item.webTitle}
           </a>
-          <hr className="mdc-list-divider"/>
+          <hr className="mdc-list-divider" />
         </div>
       );
     });
@@ -84,26 +84,26 @@ class Nav extends React.Component {
         <header className="mdc-toolbar mdc-toolbar--fixed mdc-toolbar--waterfall">
           <div className="mdc-toolbar__row">
             <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-              
+
               <span className="menu mdc-toolbar__icon mdc-toolbar__icon--menu" onClick={() => this.toggleDrawer()} >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff"  width="24" height="24" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" /></svg>
               </span>
-              
+
               <span className="back mdc-toolbar__icon mdc-toolbar__icon--menu" onClick={() => window.history.back()}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" /></svg>
               </span>
-              
+
               <span className="title mdc-toolbar__title">{this.state.title}</span>
             </section>
             <section className="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
               <div className="mdc-menu-anchor">
-                <span className="mdc-toolbar__icon mdc-toolbar__icon--menu simpleMenu" aria-label="Simple menu"  onClick={() => this.toggleMenu()}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
+                <span className="mdc-toolbar__icon mdc-toolbar__icon--menu simpleMenu" aria-label="Simple menu" onClick={() => this.toggleMenu()}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" /></svg>
                 </span>
                 <div className="mdc-simple-menu">
                   <ul className="mdc-simple-menu__items mdc-list" role="menu" aria-hidden="true">
                     <li className="mdc-list-item update" role="menuitem">Update</li>
-                    <li className="mdc-list-item" role="menuitem"  onClick={() => document.location.reload(true)} >Reload</li>
+                    <li className="mdc-list-item" role="menuitem" onClick={() => document.location.reload(true)} >Reload</li>
                     {/*<li className="mdc-list-item" role="menuitem" >Delete</li>*/}
                     <li className="mdc-list-divider" role="separator"></li>
                     <li className="mdc-list-item" role="menuitem" onClick={() => this.share()} >Share...</li>
@@ -115,7 +115,7 @@ class Nav extends React.Component {
         </header>
         <aside className="mdc-temporary-drawer">
           <nav className="mdc-temporary-drawer__drawer">
-            
+
             <header className="mdc-temporary-drawer__header">
               <div className="mdc-temporary-drawer__header-content mdc-theme--primary-bg mdc-theme--text-primary-on-primary">
                 NEWS
--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -6,154 +6,170 @@ import config from '../../api/config'; // create config json file with your key 
 
 class NewsList extends React.Component {
 
-  state = { list : [] };
+  state = { list: [] };
 
-  componentDidMount(){
-    
+  componentDidMount() {
+
+    this.getFromDb = this.getFromDb.bind(this);
+    this.updateArticles = this.updateArticles.bind(this);
+
     // Get news from db
-    this.getFromDb(window.location.hash.slice(1) || 'world');
-    
-    // Listen to hash events
-    window.addEventListener('hashchange',() => { 
-      this.getFromDb(window.location.hash.slice(1) || 'world');
-    }); 
+    this.getFromDb();
+
+    // Add event Listeners
+    this.addEventListeners()
 
     this.toggleIcons();
 
     this.snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-    
-    /* Temporary hack: use refs */
-    const updateBtn = document.querySelector('.update')
-    updateBtn.addEventListener('click', () => this.updateArticles());
   }
 
-  getFromDb(sectionId){
+  componentWillUnmount() {
+    console.log("Clean Up")
+    this.removeEventListeners();
+  }
+
+  addEventListeners() {
+    window.addEventListener('hashchange', this.getFromDb);
+    this.updateBtn = document.querySelector('.update')
+    this.updateBtn.addEventListener('click', this.updateArticles);
+  }
+
+  removeEventListeners() {
+    window.removeEventListener('hashchange', this.getFromDb);
+    this.updateBtn.removeEventListener('click', this.updateArticles);
+  }
+
+  getFromDb() {
+
+    const sectionId = window.location.hash.slice(1) || 'world';
+
     this.props.db.transaction("r", this.props.db.articles, () => {
       this.props.db.articles.where('sectionId')
-      .equalsIgnoreCase(sectionId)
-      .toArray(items => { 
-        if(items.length === 0 ){ 
-          console.log('getting ' + sectionId + ' from network');
-          this.getFromNetwork(sectionId);
-        }else{
-          console.log('getting ' + sectionId + ' from db');
-          this.setState({ list: this.sortByDate(items)});
-        }
-      });
+        .equalsIgnoreCase(sectionId)
+        .toArray(items => {
+          if (items.length === 0) {
+            console.log('getting ' + sectionId + ' from network');
+            this.getFromNetwork(sectionId);
+          } else {
+            console.log('getting ' + sectionId + ' from db');
+            this.setState({ list: this.sortByDate(items) });
+          }
+        });
     }).catch(err => {
       console.error(err.stack);
     });
   }
 
-  getFromNetwork(sectionId){
+  getFromNetwork(sectionId) {
     this.getJson(sectionId)
-    .then(this.readAsJson)
-    .then(res => res.response.results)
-    .then(items => this.setListState(items))
-    .then(items => this.addToDb(items))
-    .then(() => this.updateTime(sectionId))
-    .catch(this.logError);
+      .then(this.readAsJson)
+      .then(res => res.response.results)
+      .then(items => this.setListState(items))
+      .then(items => this.addToDb(items))
+      .then(() => this.updateTime(sectionId))
+      .catch(this.logError);
   }
 
-  getJson(section){
-    const key = config.API_KEY; 
+  getJson(section) {
+    const key = config.API_KEY;
     const url = "https://content.guardianapis.com/";
     return fetch(`${url}${section}?&show-fields=all&api-key=${key}`);
   }
 
-  readAsJson(response) { 
+  readAsJson(response) {
     if (!response.ok) {
       throw Error(response.statusText);
     }
     return response.json();
   }
 
-  setListState(items){
-    this.setState({ list: this.sortByDate(items)});
+  setListState(items) {
+    this.setState({ list: this.sortByDate(items) });
     return items;
   }
 
-  addToDb(items){
+  addToDb(items) {
     this.props.db.transaction("rw", this.props.db.articles, () => {
-      items.forEach( item => {
+      items.forEach(item => {
         item.timeStamp = Date.now();
         this.props.db.articles.put(item);
       });
     }).then(() => {
       this.showToast('Articles Downloaded');
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.error(err.stack);
     });
   }
 
   // Update download time
-  updateTime(sectionId){
-    
+  updateTime(sectionId) {
+
     let dataObj;
 
-    if(!window.localStorage.downloadTimes){
+    if (!window.localStorage.downloadTimes) {
       dataObj = {};
-    }else{
+    } else {
       dataObj = JSON.parse(window.localStorage.downloadTimes);
     }
 
     dataObj[sectionId] = Date.now();
     window.localStorage.downloadTimes = JSON.stringify(dataObj);
   }
-  
-  // Update articles
-  updateArticles(){
 
-    if(!window.navigator.onLine){
+  // Update articles
+  updateArticles() {
+
+    if (!window.navigator.onLine) {
       this.showToast('OFFLINE');
       return;
     }
 
     let sectionId = window.location.hash.slice(1) || 'world';
-    
-    if(window.localStorage.downloadTimes){
+
+    if (window.localStorage.downloadTimes) {
 
       let obj = JSON.parse(window.localStorage.downloadTimes);
       let downloadTime = obj[sectionId];
 
-      if(!downloadTime){
+      if (!downloadTime) {
         this.getFromNetwork(sectionId)
-      } else if ((Date.now() - downloadTime) > 3600000)  {
+      } else if ((Date.now() - downloadTime) > 3600000) {
         this.getFromNetwork(sectionId)
       } else {
         this.showToast('Articles Up To Date');
       }
     } else {
       this.getFromNetwork(sectionId)
-    }   
-    
+    }
+
   }
 
 
-  logError(error){
+  logError(error) {
     console.log('Looks like there was a problem: \n', error);
   }
 
-  showToast(msg){
+  showToast(msg) {
     const dataObj = { message: msg };
     this.snackbar.show(dataObj);
   }
 
   // Sort list by web publication date
-  sortByDate(arr){
+  sortByDate(arr) {
     return arr.sort((a, b) => {
       let dateA = a.webPublicationDate;
       let dateB = b.webPublicationDate;
-      if(dateA > dateB) {
+      if (dateA > dateB) {
         return -1;
-      }else if(dateA < dateB){
+      } else if (dateA < dateB) {
         return 1;
       }
       return 0;
     });
   }
 
-  toggleIcons(){
+  toggleIcons() {
     let menuIcon = document.querySelector(".menu");
     let backIcon = document.querySelector(".back");
     let simpleMenuIcon = document.querySelector(".simpleMenu");
@@ -166,19 +182,19 @@ class NewsList extends React.Component {
 
     let listItems;
 
-    if(this.state.list){  
-      listItems = this.state.list.map( (item, i) => {
-        const styles = { backgroundImage: `url(${ item.fields.thumbnail })`};
-        return(
+    if (this.state.list) {
+      listItems = this.state.list.map((item, i) => {
+        const styles = { backgroundImage: `url(${item.fields.thumbnail})` };
+        return (
           <div className="mdc-layout-grid__cell" key={i}>
             <Link to={`/detail#${item.id}`}>
               <div className="mdc-card">
                 <section className="mdc-card__media card__16-9-media" style={styles}></section>
                 <section className="mdc-card__primary">
                   <h1 className="mdc-card__title mdc-card__title--medium">{item.webTitle}</h1>
-                  <h2 className="mdc-card__subtitle">{item.webPublicationDate.slice(0,10)}</h2>
-                </section>   
-              </div>  
+                  <h2 className="mdc-card__subtitle">{item.webPublicationDate.slice(0, 10)}</h2>
+                </section>
+              </div>
             </Link>
           </div>
         )
